0f634dec2f67c36c24a39e99b328b4c58e1af3fc
Alex Valdivia <alexvaldivia@gmail.com>
2018-04-04 01:14:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,21 +5,26 @@ function takeANumber(katzDeliLine,name){
 
 function currentLine(katzDeliLine)
 {
-  var response = ["The line is currently"]
+  var response = ["The line is currently "]
   
   if katzDeliLine == 0
   {
-    response.push(" empty.")
+    response.push("empty.")
+    return response
   }
   else
   {
   
     for(let i = 0; i < katzDeliLine.length; i++)
     {
-      
+      response.push(i+1)
+      response.push(". ")
+      response.push(katzDeliLine)
+      response.push(", ")
     }
-  
+    
+    response.push(".")
+    
+    return response
   }
-  
-  return 
 }
