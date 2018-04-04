52e642926e18b2de4310eca3f32eb6a29620c445
Alex Valdivia <alexvaldivia@gmail.com>
2018-04-04 01:06:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function takeANumber(katzDeliLine,name){
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+}
+
+function currentLine(katzDeliLine)
+{
+  for(let i = 0; i < katzDeliLine.length; i++)
+  {
+    // iteratively get number and name from katzDeliLine array
+  }
+  
+  
+  
+  return 
+}
